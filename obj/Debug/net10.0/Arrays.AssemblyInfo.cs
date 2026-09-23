479dee010c87001140c5610f2b1e3aca426b4647
Diego Hidalgo <Vextreme@gmail.com>
2026-09-23 10:13:50 -0500

    Nota mayor y menor

--- obj/Debug/net10.0/Arrays.AssemblyInfo.cs
+++ obj/Debug/net10.0/Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786c97248b7e101be02e0962599b11059dc41e54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f7e38110e009360ca74db4a0cf06db91fcd7e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
