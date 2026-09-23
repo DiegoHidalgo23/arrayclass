d215405ebef19c7a091eda6a8737313cb436c1d3
Diego Hidalgo <Vextreme@gmail.com>
2026-09-23 09:01:49 -0500

    Impresion de notas

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int [] notas={15,18,12,17,20};
+
+//Imprimir notas
+for(int i=0; i<notas.Length; i++)
+{
+    Console.WriteLine(notas[i]);
+}
+
+foreach (int nota in notas)
+{
+    Console.WriteLine(nota);
+}
